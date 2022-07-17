6a416532dae56e134d595fc17d4716f442c85e43
ztiaa <00ziad.ahmed@gmail.com>
2022-07-17 17:06:51 +0200

    Add manual grid creation

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 const container = document.querySelector('.grid-container');
 const pixelColorInput = document.querySelector('.pixel-color-input');
-
+const gridWidthDiv = document.querySelector('.grid-width');
+const gridHeightDiv = document.querySelector('.grid-height');
 let gridWidth = 16;
 let gridHeight = 16;
-
+let gridItem;
+let pixelColorSelected;
 
 for (let i = 0; i < gridWidth * gridHeight; i++) {
   container.appendChild(document.createElement('div')).classList.add('grid-item');
 }
 
-let pixelColorSelected;
+gridItem = document.querySelectorAll('.grid-item');
+
+gridWidthDiv.addEventListener('change', e => {
+  gridWidth = e.target.value;
+  if (gridWidth > 100) return;
+  
+  container.replaceChildren();
+
+  for (let i = 0; i < gridWidth * gridHeight; i++) {
+    container.appendChild(document.createElement('div')).classList.add('grid-item');
+  } 
+  gridItem = document.querySelectorAll('.grid-item')
+  for (let i = 0; i < gridWidth * gridHeight; i++) {
+    gridItem[i].style.width = 800/gridWidth + 'px';
+    gridItem[i].style.height = 800/gridHeight + 'px';
+  }
+  
+})
+
+gridHeightDiv.addEventListener('change', e => {
+  gridHeight = e.target.value;
+  if (gridHeight > 100) return;
+  
+  container.replaceChildren();
+
+  for (let i = 0; i < gridWidth * gridHeight; i++) {
+    container.appendChild(document.createElement('div')).classList.add('grid-item');
+  } 
+  gridItem = document.querySelectorAll('.grid-item')
+  for (let i = 0; i < gridWidth * gridHeight; i++) {
+    gridItem[i].style.width = 800/gridWidth + 'px';
+    gridItem[i].style.height = 800/gridHeight + 'px';
+  }
+})
 
 pixelColorInput.addEventListener('change', e => {
   console.log(e);
-  const gridItem = document.querySelectorAll('.grid-item');
   pixelColorSelected = e.target.value;
   for (let i = 0; i < gridWidth * gridHeight; i++) {
     gridItem[i].style.setProperty('--color',pixelColorSelected);
@@ -26,4 +60,3 @@ container.addEventListener('mouseover', e => {
   e.stopPropagation();
   e.target.style.backgroundColor = pixelColorSelected;
 })
-
